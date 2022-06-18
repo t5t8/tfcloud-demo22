386178c5700c6da09591e22e99b53418aceea05b
tapio <tapio.tuomisto@eficode.com>
2022-06-18 15:16:07 +0300

    random pets based on integer from remote state

---
+++ terraform/random_2/main.tf
@@ -0,0 +1,12 @@
+data "tfe_outputs" "random" {
+  organization = var.tfe_organization_name
+  workspace    = var.random_workspace
+}
+
+resource "random_pet" "main" {
+  count = data.tfe_outputs.random.values.random_integer
+}
+
+output "random_pet_name" {
+  value = random_pet.main.id[*]
+}
