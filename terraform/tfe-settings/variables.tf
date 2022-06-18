b0ef217053b4c3bf99b78c55f44bce17f3c00d3c
tapio <tapio.tuomisto@eficode.com>
2022-06-18 14:42:07 +0300

    base vars

---
+++ terraform/tfe-settings/variables.tf
@@ -0,0 +1,24 @@
+variable "tfe_organization_name" {
+  type        = string
+  description = "Name of tfe organization."
+}
+
+variable "tfe_organization_email" {
+  type        = string
+  description = "Email for tfe organization contact."
+}
+
+variable "tfe_organization_members" {
+  type        = list(string)
+  description = "List of members to invite to organization"
+}
+
+variable "github_pat" {
+  type        = string
+  description = "Github personal access token to connect vcs"
+}
+
+variable "github_repo_name" {
+  type        = string
+  description = "Github repository path"
+}
