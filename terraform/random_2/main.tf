53b9edaac69c4c6c52f2c151b719e303a1ab23b0
tapio <tapio.tuomisto@eficode.com>
2022-06-18 15:29:00 +0300

    fix syntax

--- terraform/random_2/main.tf
+++ terraform/random_2/main.tf
@@ -8,6 +8,5 @@ resource "random_pet" "main" {
 }
 
 output "random_pet_name" {
-  count = data.tfe_outputs.random.values.random_integer
-  value = random_pet.main.id[count.index]
+  value = random_pet.main.[*].id
 }
