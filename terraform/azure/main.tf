82579e6cabffa851d8cc0b1b06b2a1c2c473f938
tapio <tapio.tuomisto@eficode.com>
2022-06-20 09:55:41 +0300

    set sku from var

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -10,5 +10,5 @@ resource "azurerm_service_plan" "demo" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   os_type             = "Linux"
-  sku_name            = "B2"
+  sku_name            = var.app_service_sku
 }
