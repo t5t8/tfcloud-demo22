8918551fad33096111f67017e099b6ac1db92a0c
Tapio Tuomisto <59526874+t5t8@users.noreply.github.com>
2022-06-20 09:59:56 +0300

    add webapp plan (#3)
    * add webapp plan
    
    * use service_plan resource instead of depracated
    
    * change syntax and sku to B2
    
    * remove reserved argument
    
    * set sku from var
    
    * fix condition

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -2,3 +2,13 @@ resource "azurerm_resource_group" "main" {
   name     = "${var.prefix}-main"
   location = "westeurope"
 }
+
+
+## Example webapp plan
+resource "azurerm_service_plan" "demo" {
+  name                = "${var.prefix}-plan"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  os_type             = "Linux"
+  sku_name            = var.app_service_sku
+}
--- terraform/azure/variables.tf
+++ terraform/azure/variables.tf
@@ -3,3 +3,13 @@ variable "prefix" {
   description = "Prefix all resources"
   default     = "TFCLOUDDEMO062022"
 }
+
+variable "app_service_sku" {
+  type        = string
+  description = "SKU for App service. Defaults to B1, P1v3 is other option"
+  default     = "B1"
+  validation {
+    condition     = var.app_service_sku == "B1" || var.app_service_sku == "P1v3"
+    error_message = "Only B1 or P1v3 are accepted."
+  }
+}
