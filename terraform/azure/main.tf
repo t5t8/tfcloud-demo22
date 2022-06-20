291ee6ac893b9112a873d45d804d643aa44c0c41
tapio <tapio.tuomisto@eficode.com>
2022-06-20 09:40:08 +0300

    add webapp plan

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -2,3 +2,17 @@ resource "azurerm_resource_group" "main" {
   name     = "${var.prefix}-main"
   location = "westeurope"
 }
+
+
+## Example webapp plan
+resource "azurerm_app_service_plan" "demo" {
+  name                = "${var.prefix}-plan"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  kind                = "linux"
+  reserved            = true
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
