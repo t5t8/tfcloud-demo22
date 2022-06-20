4b4f5740de73a95949b31ebf50194f6de0e6943c
tapio <tapio.tuomisto@eficode.com>
2022-06-20 09:56:07 +0300

    fix condition

--- terraform/azure/variables.tf
+++ terraform/azure/variables.tf
@@ -9,7 +9,7 @@ variable "app_service_sku" {
   description = "SKU for App service. Defaults to B1, P1v3 is other option"
   default     = "B1"
   validation {
-    condition     = var.app_service_sku = "B1" || var.app_service_sku == "P1v3"
+    condition     = var.app_service_sku == "B1" || var.app_service_sku == "P1v3"
     error_message = "Only B1 or P1v3 are accepted."
   }
 }
