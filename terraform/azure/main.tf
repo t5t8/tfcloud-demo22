06b4ee8043f6c9ae38d237f211e54df14062b95c
tapio <tapio.tuomisto@eficode.com>
2022-06-20 09:50:39 +0300

    remove reserved argument

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -9,7 +9,6 @@ resource "azurerm_service_plan" "demo" {
   name                = "${var.prefix}-plan"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  reserved            = true
   os_type             = "Linux"
   sku_name            = "B2"
 }
