0d081052d83bb14712b9daf2dc0559e5fd788701
tapio <tapio.tuomisto@eficode.com>
2022-06-20 09:49:04 +0300

    change syntax and sku to B2

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -9,10 +9,7 @@ resource "azurerm_service_plan" "demo" {
   name                = "${var.prefix}-plan"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  kind                = "linux"
   reserved            = true
-  sku {
-    tier = "Basic"
-    size = "B1"
-  }
+  os_type             = "Linux"
+  sku_name            = "B2"
 }
