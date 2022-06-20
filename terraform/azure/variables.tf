6869543d92dfec0007513570fd98c702db31470f
tapio <tapio.tuomisto@eficode.com>
2022-06-20 10:34:28 +0300

    change default

--- terraform/azure/variables.tf
+++ terraform/azure/variables.tf
@@ -7,7 +7,7 @@ variable "prefix" {
 variable "app_service_sku" {
   type        = string
   description = "SKU for App service."
-  default     = "B1"
+  default     = "P1v2"
   validation {
     condition     = var.app_service_sku == "P1v2" || var.app_service_sku == "P1v2"
     error_message = "Only B1 or P1v2 are accepted."
