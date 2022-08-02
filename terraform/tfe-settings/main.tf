d784265b77a936010d6eb6218157d59580b9d08e
tapio <tapio.tuomisto@eficode.com>
2022-08-02 15:11:58 +0300

    add team plan

--- terraform/tfe-settings/main.tf
+++ terraform/tfe-settings/main.tf
@@ -3,12 +3,36 @@ resource "tfe_organization" "main" {
   email = var.tfe_organization_email
 }
 
+## TFE team and membership resources require entitlement to create teams and this
+## can not be managed via tfe provider. You need to enable either free trial or team level
+## plan in app.terraform.io
+
+resource "tfe_team" "demo" {
+  name         = "tfe_demo_team"
+  organization = tfe_organization.main.name
+  organization_access {
+    manage_modules          = false
+    manage_policies         = false
+    manage_policy_overrides = false
+    manage_providers        = false
+    manage_run_tasks        = false
+    manage_vcs_settings     = false
+    manage_workspaces       = true
+  }
+}
 
 
 resource "tfe_organization_membership" "main" {
   for_each     = toset(var.tfe_organization_members)
   organization = tfe_organization.main.name
   email        = each.value
+}
+
+
+resource "tfe_team_organization_member" "main" {
+  for_each                   = toset(var.tfe_organization_members)
+  team_id                    = tfe_team.demo.id
+  organization_membership_id = tfe_organization_membership.main[each.key].id
 }
 
 
