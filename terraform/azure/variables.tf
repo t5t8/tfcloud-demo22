6917f55b347aa687957d770249e63f973e1b7565
tapio <tapio.tuomisto@eficode.com>
2022-08-19 09:35:12 +0300

    return variable

--- terraform/azure/variables.tf
+++ terraform/azure/variables.tf
@@ -4,12 +4,12 @@ variable "prefix" {
   default     = "TFCLOUDDEMO062022"
 }
 
-# variable "app_service_sku" {
-#   type        = string
-#   description = "SKU for App service. Defaults to B1, P1v3 is other option"
-#   default     = "B1"
-#   validation {
-#     condition     = var.app_service_sku == "B1" || var.app_service_sku == "P1v3"
-#     error_message = "Only B1 or P1v3 are accepted."
-#   }
-# }
+variable "app_service_sku" {
+  type        = string
+  description = "SKU for App service. Defaults to B1, P1v3 is other option"
+  default     = "B1"
+  validation {
+    condition     = var.app_service_sku == "B1" || var.app_service_sku == "P1v3"
+    error_message = "Only B1 or P1v3 are accepted."
+  }
+}
